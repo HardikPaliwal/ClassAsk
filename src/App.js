d4b560d1f63325b99bf5f26097aab27cc1b76a8a
HardikPaliwal <hardikpaliwal1999@gmail.com>
2019-09-07 22:05:32 -0400

    fixed merge shit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import React from "react";
 import ClassCodeForm from "./components/ClassCodeForm";
 import { Grommet } from "grommet";
 import { grommet } from "grommet/themes";
+import Home from "./scenes/Home/Home"
 import QuestionPage from "./components/QuestionPage";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+function land(){
+  return ( 
+  <Grommet theme={grommet} full>
+    <ClassCodeForm/>
+  </Grommet>
+  )
+}
 
 function App() {
   return (
-    <Grommet theme={grommet} full>
-      <ClassCodeForm/>
-    </Grommet>
+    <Route path="/" exact component={land} />
+    <Route path="/classroom" component={QuestionPage} />
+   
   );
 }
 
