61d0e22377eb9c559d5fb7444040e336c62441b9
Smit Rao <smit.rao@mail.utoronto.ca>
2019-09-08 01:00:43 -0400

    add stuff

--- src/App.js
+++ src/App.js
@@ -1,33 +1,30 @@
-import React, { Component } from "react";
+import React from "react";
 import ClassCodeForm from "./components/ClassCodeForm";
 import { Grommet } from "grommet";
 import { grommet } from "grommet/themes";
-import {Home, Session } from "./scenes"
+import { Home, Session } from "./scenes";
 import QuestionPage from "./components/QuestionPage";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-function land(){
-  return ( 
-  <Grommet theme={grommet} full>
-    <ClassCodeForm/>
-  </Grommet>
-  )
+function land() {
+  return (
+    <Grommet theme={grommet} full>
+      <ClassCodeForm />
+    </Grommet>
+  );
 }
 
 function App() {
   return (
     <Router>
-    <div>
-      <Route path="/" exact component={land} />
-      <Route path="/student" component={QuestionPage} />
-      <Route path="/classroom" component={Session} />
-      <Route path="/Home" component={Home} />
-    </div>
-  </Router>
-   
-   
+      <div>
+        <Route path="/" exact component={land} />
+        <Route path="/student" component={QuestionPage} />
+        <Route path="/classroom" component={Session} />
+        <Route path="/Home" component={Home} />
+      </div>
+    </Router>
   );
-
 }
 
 export default App;
