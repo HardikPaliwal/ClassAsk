8dfb3f3270b2799cada80f9265e3cc579aa6c93a
Smit Rao <31007269+SmitRao@users.noreply.github.com>
2019-09-08 01:16:02 -0400

    add stuff (#7)

--- src/components/ClassCodeForm.js
+++ src/components/ClassCodeForm.js
@@ -1,29 +1,40 @@
 import React, { Component } from "react";
 import "./component-styles/keyframes.css";
-import { Form, FormField, Button, Box, Image, Anchor } from "grommet";
-import ProfLoginFrom from "./ProfLoginForm"
+import { Form, FormField, Button, Box, Image, Anchor, Text } from "grommet";
+
 class ClassCodeForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
       loginFlow: "student",
-      bottomText : "Not a student? Click here for professors' login",
-      topText: "Sign Up",
+      bottomText: "Not a student? Click here for professors' login.",
+      topText: "Sign In"
     };
   }
   onClickBottomText(state) {
-    if(this.state.loginFlow == "student"){
-      this.setState({loginFlow: "signIn", bottomText: "Don't have an account? Click here to sign up now!"});
-    } else{
-      if(this.state.loginFlow == "signIn")
-        this.setState({loginFlow: "signUp", topText:"Sign Up", bottomText:"Sign in instead"});
+    if (this.state.loginFlow == "student") {
+      this.setState({
+        loginFlow: "signIn",
+        bottomText: "Don't have an account? Click here to sign up now!"
+      });
+    } else {
+      if (this.state.loginFlow == "signIn")
+        this.setState({
+          loginFlow: "signUp",
+          topText: "Sign Up",
+          bottomText: "Sign in instead."
+        });
       else
-        this.setState({loginFlow:  "signIn", topText: "Sign In", bottomText: "Don't have an account? Click here to sign up now!"});
+        this.setState({
+          loginFlow: "signIn",
+          topText: "Sign In",
+          bottomText: "Don't have an account? Click here to sign up now!"
+        });
     }
   }
 
   changeProfLogin() {
-    this.setState({login: !this.state.login});
+    this.setState({ login: !this.state.login });
   }
   render() {
     return (
@@ -53,39 +64,55 @@ class ClassCodeForm extends Component {
           <Box height="small" width="medium">
             <Image fit="contain" src="ClassAsk.png" />
           </Box>
-          {this.state.loginFlow == "student" ? 
-          <Form align="center" pad="large">
-            <FormField
-              style={{ "text-align": "center" }}
-              color="gray"
-              name="name"
-              placeholder="ex. 123456"
-              label="Class Code"
-            />
-            <Button type="submit" primary color="#000000" label="Submit" />
-            </Form>
-                        : ""}
-
-          {this.state.loginFlow == "signIn" ||this.state.loginFlow == "signUp"?
+          {this.state.loginFlow == "student" ? (
             <Form align="center" pad="large">
-            <div>{this.state.topText}</div>
-            <FormField
-              style={{ "text-align": "center" }}
-              color="gray"
-              name="name"
-              placeholder="ex. John Smith"
-              label = "Username and Password"
-            />
-            <FormField
-              style={{ "text-align": "center" }}
-              color="gray"
-              name="name"
-              placeholder="ex. password"
-            />
+              <FormField
+                style={{ "text-align": "center" }}
+                color="gray"
+                name="name"
+                placeholder="ex. 123456"
+                label="Class Code"
+              />
               <Button type="submit" primary color="#000000" label="Submit" />
-              </Form>
-            : " "}
-          <div onClick={this.onClickBottomText.bind(this)}href="#">{this.state.bottomText}</div>
+            </Form>
+          ) : (
+            ""
+          )}
+
+          {this.state.loginFlow == "signIn" ||
+          this.state.loginFlow == "signUp" ? (
+            <Box align="center" pad="large">
+              <Text weight="bold" size="large">
+                {this.state.topText}
+              </Text>
+              <Box pad="medium">
+                <Form>
+                  <FormField
+                    style={{ "text-align": "center" }}
+                    color="gray"
+                    name="name"
+                    placeholder="ex. Joe_1988"
+                    label="Username"
+                  />
+                </Form>
+                <Form>
+                  <FormField
+                    style={{ "text-align": "center" }}
+                    color="gray"
+                    name="name"
+                    placeholder="******"
+                    label="Password"
+                  />
+                </Form>
+                <Button type="submit" primary color="#000000" label="Submit" />
+              </Box>
+            </Box>
+          ) : (
+            " "
+          )}
+          <div onClick={this.onClickBottomText.bind(this)} href="#">
+            <Anchor href="#">{this.state.bottomText}</Anchor>
+          </div>
         </Box>
       </Box>
     );
