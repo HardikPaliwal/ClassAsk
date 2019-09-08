06b1edfc45ef4108a3ad3765a7bdf8b0a2903ec1
Smit Rao <smit.rao@mail.utoronto.ca>
2019-09-07 22:09:01 -0400

    Add state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import ClassCodeForm from "./components/ClassCodeForm";
 import { Grommet } from "grommet";
 import { grommet } from "grommet/themes";
 import QuestionPage from "./components/QuestionPage";
+import ProfLoginForm from "./components/ProfLoginForm";
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showProfLogin: false,
+    };
+  }
 
-function App() {
-  return (
-    <Grommet theme={grommet} full>
-      <ClassCodeForm/>
-    </Grommet>
-  );
+  changeProfLogin() {
+    this.setState({showProfLogin: !this.state.showProfLogin});
+  }
+  
+  render() {
+    return (
+      <Grommet theme={grommet} full>
+        {this.state.showProfLogin ? <ProfLoginForm /> : <ClassCodeForm />}:
+      </Grommet>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/ProfLoginForm.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import "./component-styles/keyframes.css";
+import { Form, FormField, Button, Box, Image, Anchor } from "grommet";
+
+class ProfLoginForm extends Component {
+  render() {
+    return (
+      <Box fill pad="medium">
+        <Box
+          fill
+          className="dropped-box"
+          gridArea="greeter"
+          gap="medium"
+          flex
+          border="left"
+          basis="large"
+          alignContent="center"
+          a11yTitle="ClassAsk box for prof login"
+          alignSelf="center"
+          elevation="medium"
+          wrap
+          margin="medium"
+          pad="small"
+          overflow="auto"
+          responsive
+          round="medium"
+          justify="center"
+          align="center"
+          background="#ffffffdd"
+        >
+          <Box height="small" width="medium">
+            <Image fit="contain" src="ClassAsk.png" />
+          </Box>
+          <Form align="center" pad="large">
+            <FormField
+              style={{ "text-align": "center" }}
+              color="gray"
+              name="name"
+              label="Username"
+              placeholder="ex. PennyApp99"
+            />
+          </Form>
+          <Form align="center" pad="large">
+            <FormField
+              style={{ "text-align": "center" }}
+              color="gray"
+              name="name"
+              label="Password"
+              placeholder="********"
+            />
+            <Button pad="large" type="submit" primary color="#000000" label="Submit" />
+          </Form>
+          <Anchor href="#">Not a professor? Click here to enter a class code.</Anchor>
+        </Box>
+      </Box>
+    );
+  }
+}
+
+export default ProfLoginForm;
